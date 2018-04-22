8f66095e06fae39e2365e5731f1292c6324105f4
ronthecookie <ronthecookie0101@gmail.com>
2018-04-22 14:40:06 +0300

    add more things

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ module.exports = class DiscordboatsClub {
     constructor(options = {}) {
         if (!options.token) throw new Error("You must pass in a token to the DiscordboatsClub constructor");
         this.token = options.token;
+        // test the token:
+        snek.get(base).set("Authorization", options.token).then(res => {
+            if (res.status == 400 || res.status == 403) throw new Error("Invalid API Key");
+        });
     }
     /**
      * Post your guild count to the Discordboats.club Public API.
@@ -16,6 +20,30 @@ module.exports = class DiscordboatsClub {
      * @returns {Promise} Promise that resolves if the request went through.
      */
     async postGuilds(server_count = 0) {
-        await snek.get(base + "/bot/stats").set("Authorization", this.token).send({server_count});
+        await snek.post(base + "/bot/stats").set("Authorization", this.token).send({server_count});
+    }
+    /**
+     * Get a user.
+     * @param {String} id 
+     */
+    async getUser(id) {
+        const res = await snek.get(base + "/user/" + id).set("Authorization", this.token).send();
+        if (res.status == 200) {
+            return res.body.data;
+        } else {
+            throw new Error(res.body.error || res.status);
+        }
+    }
+    /**
+     * Get a bot.
+     * @param {String} id Or "me" if you want to get yourself.
+     */
+    async getBot(id) {
+        const res = await snek.get(base + "/bot/" + id).set("Authorization", this.token).send();
+        if (res.status == 200) {
+            return res.body.data;
+        } else {
+            throw new Error(res.body.error || res.status);
+        }
     }
 }
